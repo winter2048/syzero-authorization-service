c94707fc94988107f3bd10c1030091d97bd01abd
winter2048 <522112669@qq.com>
2023-03-18 23:17:06 +0800

    update

---
+++ .github/workflows/script/set-buildnumber.ps1
@@ -0,0 +1,43 @@
+param(
+    [int]$major,
+    [int]$minor,
+    [int]$patch,
+    [string]$tag,
+    [string]$refName
+)
+
+$version = Get-Content "$PSScriptRoot\..\.version"
+if (!$major) {
+    $major = $version.Split(".")[0]
+}
+if (!$minor) {
+    $minor = $version.Split(".")[1]
+}
+if (!$patch) {
+    $patch = $version.Split(".")[2]
+}
+
+$ProductMajorVersion = $major
+$ProductMinorVersion = $minor
+$ProductpatchVersion = $patch
+$MyCustomBuildVersion = "$ProductMajorVersion.$ProductMinorVersion.$ProductpatchVersion"
+
+if ($refName) {
+    $tag = $refName.Replace("_","-")
+}
+if ($tag -and $tag -ne "master") {
+    $BaselineYear = 2023
+    $CurrentDate = (Get-Date).ToUniversalTime()
+    $StartOfDay = Get-Date -Date $CurrentDate -Hour 0 -Minute 00 -Second 00
+    $BuildMajorVersion = ($CurrentDate.Year - $BaselineYear) * 12 + $CurrentDate.Month
+    $BuildMajorVersion = $BuildMajorVersion * 31 + $CurrentDate.Day
+    $BuildMinorVersion = [math]::floor(((New-TimeSpan -Start $StartOfDay -End $CurrentDate).TotalSeconds) / 2)
+    $MyCustomBuildVersion = "$MyCustomBuildVersion-$tag.$BuildMajorVersion.$BuildMinorVersion"
+}
+$env:BUILD_BUILDNUMBER = $MyCustomBuildVersion
+[Environment]::SetEnvironmentVariable("BUILD_BUILDNUMBER", $MyCustomBuildVersion, "Machine")
+echo "BUILD_BUILDNUMBER=$MyCustomBuildVersion" >> $env:GITHUB_OUTPUT
+echo "BUILD_BUILDNUMBER=$MyCustomBuildVersion" >> $env:GITHUB_ENV
+Write-Host "Setting the value of current build version :  $MyCustomBuildVersion"
+
+dir env:
